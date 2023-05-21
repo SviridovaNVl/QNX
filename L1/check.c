a3586a2d5c5045b14c0423a24e9e86884d39e53d
SviridovaNVl <134208388+SviridovaNVl@users.noreply.github.com>
2023-05-21 15:47:08 +0300

    Add files via upload

---
+++ L1/check.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char buf[100];
+	char truename[] = "Sviridova Nina I992";
+	while (gets(buf))
+	{
+		if (strcmp(truename, buf) == 0)
+		{
+			printf("Name is correct \n");
+			return 0;
+		}
+		else
+		{
+			printf("Name is incorrect \n");
+			return 1;
+		}
+	}
+}
---
+++ L1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Sviridova Nina I992");
+	printf("\n");
+	return 0;
+}
